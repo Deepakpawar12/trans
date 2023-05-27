3f7b8a8d1fb348b8bf0d8521abd9d2bdfef4fc79
admin <admin@DESKTOP-TJSP1RG>
2023-05-27 12:04:52 +0530

    updated overriding

--- trans/src/d2/Main1.java
+++ trans/src/d2/Main1.java
@@ -1,5 +1,7 @@
 package d2;
 
+import d2.Hr.Ss;
+
 class Hr {
 
 	void nurse(int a, int b) {
@@ -9,6 +11,21 @@ class Hr {
 	void doc(int x, int y, int z) {
 		System.out.println(x + y - z);
 	}
+
+	class Ss extends Hr {
+
+		@Override
+		void doc(int x, int y, int z) {
+			// TODO Auto-generated method stub
+			super.doc(x, y, z);
+		}
+
+		@Override
+		void nurse(int a, int b) {
+			// TODO Auto-generated method stub
+			super.nurse(a, b);
+		}
+	}
 }
 
 public class Main1 {
@@ -16,6 +33,7 @@ public class Main1 {
 		Hr h = new Hr();
 		h.doc(50000, 5550, 2220);
 		h.nurse(20000, 100);
+		// Ss ss = new Ss();
 
 	}
 }
